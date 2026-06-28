514c24cd5aa25dec6afe033eeb3904600237ff65
ozindoye <ozindoye@gmail.com>
2026-06-28 22:37:39 +0100

    Initial scaffold: Spring Boot with Web, JPA, H2, Validation

---
+++ src/main/java/com/ousman/personal_finance_dashboard/PersonalFinanceDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ousman.personal_finance_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ousman/personal_finance_dashboard/PersonalFinanceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ousman.personal_finance_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
